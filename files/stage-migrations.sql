ed1a1e0c3d4226657db4020a3c000d9869cdfa1e
Stefano Rutishauser <rutishauser@mediamotion.ch>
2019-12-19 08:22:41 +0100

    TASK | Add sql migration for language shops

--- files/stage-migrations.sql
+++ files/stage-migrations.sql
@@ -3,6 +3,10 @@
 #add /stage to base_path
 UPDATE s_core_shops SET base_path = IF(s_core_shops.base_path IS NULL, '/stage', CONCAT(s_core_shops.base_path, '/stage'));
 
+#add /stage to base_url for language shops
+UPDATE s_core_shops SET base_url = IF(s_core_shops.base_url IS NOT NULL, CONCAT('/stage',s_core_shops.base_url), NULL);
+
+
 #enable maintance mode
 UPDATE s_core_config_elements SET value = 'b:1;' WHERE name='setoffline';
 
