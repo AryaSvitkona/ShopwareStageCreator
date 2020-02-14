b022c5fb7d014d95e7f40718075e333159939a3b
Stefano Rutishauser <rutishauser@mediamotion.ch>
2020-02-14 20:57:22 +0100

    TASK | Add update function worksteps for further development

--- commands/update.sh
+++ commands/update.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
 echo "Script not written until now"
+
+#creates new logfile
+
+#ask for override permission
+
+#ask if database sync (drop, create, import)
+
+#check if stage exists (folder, shopware.php, config.php)
+
+#collect mysql credentials
+
+#check mysql connection
+
+#rsync from live to stage without config.php
+
+#create dump of live database
+
+#drop stage database
+
+#import live to stage database
+
+#run stage-migrations
+
+#
