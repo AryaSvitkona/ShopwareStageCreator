26ccd45e68a14074a2916c1ce52d4f05372acea9
Stefano Rutishauser <rutishauser@mediamotion.ch>
2020-03-06 14:31:17 +0100

    TASK | Add  message highlighting while delete instance prompt

--- commands/delete.sh
+++ commands/delete.sh
@@ -4,7 +4,7 @@ function promptDeleteStage(){
 
     collectStageCredentials
 
-    deleteInstance=${deleteInstance:-$(promptYesOrNo "Are you sure? This will delete the whole stage instance in ${warn}$shopDir/stage${reset} and clear database ${Db_Database_Stage}! (y/N)" 'n')}
+    deleteInstance=${deleteInstance:-$(promptYesOrNo "Are you sure? This will delete the whole stage instance in ${warn}$shopDir/stage${reset} and clear database ${warn}${Db_Database_Stage}${reset}! (y/N)" 'n')}
     if [ ${deleteInstance} = "n" ]; then
         echo -e "\n${lightGreen}Not touching the stage instance, have fun!${reset}\n"
         exit 0;
